f11bb1da625896bd2feb994bfc590c35d7460f69
Nixon Gamboa <nrgamboa97@gmail.com>
2021-05-26 01:03:24 -0500

    Se crea nueva clase Bar en main.js y se instancian esas 2 barras

--- main.js
+++ main.js
@@ -5,12 +5,13 @@
         this.playing= false;
         this.game_over= false;
         this.bars = [];
+        this.ball = null;
     }
 
     self.Board.prototype = {
         get elements(){
             var elements = this.bars;
-            elements.push(ball);
+            elements.push(this.ball);
             return elements;
         }
     }
@@ -22,7 +23,47 @@
         this.canvas.width = board.width;
         this.canvas.height = board.height;
         this.board = board;
-        this.cxt = canvas.getContext("2d");
+        this.ctx = canvas.getContext("2d");
+    }
+
+    self.BoardView.prototype = {
+        draw: function(){
+            for(var i = this.board.elements.length-1; i>=0; i--){
+                var el = this.board.elements[i];
+
+                draw (this.ctx,el);
+            }
+        }
+    }
+
+    function draw (ctx, element){
+        if (element !== null && element.hasOwnProperty("kind")){
+            switch (element.kind){
+                case "rectangle":
+                    ctx.fillRect(element.x, element.y,element.width, element.height);
+                    break;
+            }
+        }
+    }
+})();
+
+(function(){
+    self.Bar = function(x,y,width,height,board){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.board = board;
+        this.board.bars.push(this);
+        this.kind = "rectangle";
+    }
+    self.Bar.prototype ={
+        down: function(){
+
+        },
+        up: function(){
+
+        }
     }
 })();
 
@@ -31,7 +72,10 @@ window.addEventListener("load", main);
 function main(){
     var board = new Board(800,400);
     console.log(board);
+    var bar = new Bar (20,150,20,100,board)
+    var bar = new Bar (760,150,20,100,board)
     var canvas = document.getElementById('canvas');
     var board_view = new BoardView(canvas, board);
 
+    board_view.draw();
 }
