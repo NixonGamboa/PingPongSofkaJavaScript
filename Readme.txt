43bdc38b1083113040b234db60c421d36fce6eb6
Nixon Gamboa <nrgamboa97@gmail.com>
2021-05-26 23:06:59 -0500

    Se agregó prototipo Ball, tambien se da movimiento a las barras usando canva.clean

---
+++ Readme.txt
@@ -0,0 +1,11 @@
+
+Preguntas sobre la practica.
+
+1. Como se capturan las interacciones en tiempo real con el teclado y como reconocer la tecla presionada.
+R// Al lomento de presionar una tecla se genera un evento, este evento puede ser escuchado por el metodo addEvenListener de document y retorna informacion sobre el evento, si buscamos en el atributo keyCode podemos comparar con el de la letra que necesitamos y ejercer alguna decision.
+
+2. Que significa la expresion "this." en prototipos javascript?
+R//Hace referencia a los atributos y metodos que componen las caracteristicas de un prototipo, lo describen como cualidades y comportamientos y debe ser invocada si se pretende llamar uno de estos dentro de si mismo.
+
+3.Que significa una funcion recursiva?
+R// Es una funcion que en su ejecucion se invoca a si misma.
--- main.js
+++ main.js
@@ -27,22 +27,33 @@
     }
 
     self.BoardView.prototype = {
+        clean: function(){
+            this.ctx.clearRect(0,0,this.board.width,this.board.height)
+        },
         draw: function(){
             for(var i = this.board.elements.length-1; i>=0; i--){
                 var el = this.board.elements[i];
 
                 draw (this.ctx,el);
             }
+        },
+        play: function (){
+            this.clean();
+            this.draw();
         }
     }
-
+    
     function draw (ctx, element){
-        if (element !== null && element.hasOwnProperty("kind")){
-            switch (element.kind){
-                case "rectangle":
-                    ctx.fillRect(element.x, element.y,element.width, element.height);
-                    break;
-            }
+        switch (element.kind){
+            case "rectangle":
+                ctx.fillRect(element.x, element.y,element.width, element.height);
+                break;
+            case "circle":
+                ctx.beginPath();
+                ctx.arc(element.x,element.y, element.radius,0,7);
+                ctx.fill();
+                ctx.closePath();
+                break;
         }
     }
 })();
@@ -56,7 +67,7 @@
         this.board = board;
         this.board.bars.push(this);
         this.kind = "rectangle";
-        this.speed= 10;
+        this.speed= 20;
 
     }
     self.Bar.prototype ={
@@ -73,27 +84,48 @@
     }
 })();
 
+(function(){
+    self.Ball = function(x,y,radius,board){
+        console.log("constructor ball")
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.speed_y = 0;
+        this.speed_x = 3;
+        this.board = board;
+        board.ball = this;
+        this.kind = "circle";
+    }
+})();
+
 var board = new Board(800,400);
 console.log(board);
-var bar = new Bar (20,150,20,100,board)
-var bar = new Bar (760,150,20,100,board)
+var bar = new Bar (20,150,10,100,board)
+var bar2 = new Bar (770,150,10,100,board)
 var canvas = document.getElementById('canvas');
 var board_view = new BoardView(canvas, board);
+var ball = new Ball(350,200,7,board);
 
 
 document.addEventListener("keydown", function (ev){
-    console.log(ev.keyCode)
+    ev.preventDefault();
     if(ev.keyCode == 38){
-        bar.up();
+        bar2.up();
     }else if (ev.keyCode == 40){
+        bar2.down();
+    }
+    if(ev.keyCode == 87){
+        bar.up();
+    }else if (ev.keyCode == 83){
         bar.down();
     }
-    console.log(bar.toString());
 });
 
-window.addEventListener("load", main);
+//window.addEventListener("load", main);
+window.requestAnimationFrame(controller);
 
-function main(){
-    
-    board_view.draw();
+
+function controller(){
+    board_view.play();
+    window.requestAnimationFrame(controller);
 }
